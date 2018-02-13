aa5c45241ae82cd4821c73f8d3662746efe20ac4
IlyaFed <ilya.d.fedorov@phystech.edu>
2018-02-13 22:13:42 +0300

    some fix

--- start.py
+++ start.py
@@ -8,7 +8,7 @@ from lib.pt_diagram import *
 
 path = './test/all'
 logfile = './test/log.log'
-objects = [coordinate_visualisation()
+objects = [coordinate_visualisation(),
            pt_diagram(),
            temperature(),
            energy_distribution_ion('potential'), energy_distribution_ion('kinetic'),
