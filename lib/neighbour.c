bb34ffd73978ad13927d257bac9237cc25009005
IlyaFed <ilya.d.fedorov@phystech.edu>
2018-02-21 14:47:52 +0300

    some fix

--- lib/neighbour.c
+++ lib/neighbour.c
@@ -84,7 +84,7 @@ int* neighbour_list( double* x, double* y, double* z, double* type, double* wall
     return list;
 }
 
-int find_neighbour( int* list, int* k_list_ion, int* k_list_el,
+void find_neighbour( int* list, int* k_list_ion, int* k_list_el,
     double* x, double* y, double* z,
     double* x_2, double* y_2, double* z_2,
     double* type, double* wall, double cut, int n,
@@ -167,112 +167,8 @@ int* neighbour_list_two( double* x, double* y, double* z,
             list[n*2 + 6] ++;
     }
 
-    for (int i = 0; i < k; i++)
-        std::cerr << k_list_ion[i] << " "<< k_list_el[i] << "\n";
-    delete[] k_list_el;
-    delete[] k_list_ion;
-    return list;
-}
-
-
-// TODO
-int* neighbour_list_two_better_one( double* x, double* y, double* z, double* x_2, double* y_2, double* z_2, double* type, double* wall, double cut, int n){
-    int* list = new int[n*2 + 7]; // here we will return bonding molecules[n], types of every particle[n], distribution[7 (e, H+, H, H2+, H2, H3+, H3]
-    int* k_list_ion = new int[n];
-    int* k_list_el = new int[n];
-    int* list_2 = new int[n];
-    cut = cut*cut; // we will compare dist^2
-    for (int l = 0; l < n*2 + 7; l ++)
-        list[l] = 0;
-
-    for (int l = 0; l < n; l ++){
-        list_2[l] = 0;
-        k_list_el[l] = 0;
-        k_list_ion[l] = 0;
-    }
-
-    double dist;
-    int k = 1;
-    for (int i = 0; i < n; i ++){
-        if (list[i] == 0){
-            list[i] = k;
-            k++;
-            if (type[i] == 1.0)
-                k_list_ion[list[i]] ++;
-
-            if (type[i] == 2.0)
-                k_list_el[list[i]] ++;
-        }
-        for (int j = i+1; j < n; j++){
-            dist = get_r(x[i], y[i], z[i], x[j], y[j], z[j],  wall);
-            if (dist < cut){
-                list[j] = list[i];
-                if (type[j] == 1.0)
-                    k_list_ion[list[j]] ++;
-                if (type[j] == 2.0)
-                    k_list_el[list[j]] ++;
-            }
-        }
-    }
-    int k_2 = 1;
-    for (int i = 0; i < n; i ++){
-        if (list_2[i] == 0){
-            list_2[i] = k_2;
-            k_2++;
-        }
-        for (int j = i+1; j < n; j++){
-            dist = get_r(x_2[i], y_2[i], z_2[i], x_2[j], y_2[j], z_2[j],  wall);
-            if (dist < cut){
-                list_2[j] = list_2[i];
-            }
-        }
-    }
-
-    for (int i = 0; i < n; i ++){
-        for (int j = i+1; j < n; j ++){
-            if ( list[i] == list[j] ){
-                if ( list_2[i] != list_2[j] ){
-                    k_list_el[list[i]] = 0;
-                    k_list_ion[list[i]] = 0;
-                }
-            }
-        }
-    }
-
-    for (int i = 0; i < n; i++){
-        list[i+n] = k_list_ion[list[i]];
-    }
-
-    int el, ion;
-    for (int j = 0; j < k; j++){
-        el = k_list_el[j];
-        ion = k_list_ion[j];
-        // e
-        if ( (el == 1) && (ion == 0))
-            list[n*2 + 0] ++;
-        // H+
-        if ( (el == 0) && (ion == 1))
-            list[n*2 + 1] ++;
-        // H
-        if ( (el == 1) && (ion == 1))
-            list[n*2 + 2] ++;
-        // H2+
-        if ( (el == 1) && (ion == 2))
-            list[n*2 + 3] ++;
-        // H2
-        if ( (el == 2) && (ion == 2))
-            list[n*2 + 4] ++;
-        // H3+
-        if ( (el == 2) && (ion == 3))
-            list[n*2 + 5] ++;
-        // H3
-        if ( (el == 3) && (ion == 3))
-            list[n*2 + 6] ++;
-    }
-
-    for (int i = 0; i < k; i++)
-        std::cerr << k_list_ion[i] << " "<< k_list_el[i] << "\n";
-    delete[] list_2;
+//    for (int i = 0; i < k; i++)
+//        std::cerr << k_list_ion[i] << " "<< k_list_el[i] << "\n";
     delete[] k_list_el;
     delete[] k_list_ion;
     return list;
