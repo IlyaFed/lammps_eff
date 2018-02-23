58c21e7b542c37ff9164e9d2697083e0d8862505
IlyaFed <ilya.d.fedorov@phystech.edu>
2018-02-23 20:08:19 +0300

    add h3 energy by distance

--- lib/energy_h3.py
+++ lib/energy_h3.py
@@ -2,13 +2,13 @@
 from lib.common_object import *
 import numpy as np
 
-class energy(dash_object):
+class energy_h3(dash_object):
     def load_step(self, args):
-        parametrs = args['parametrs']
-        Step = args['Step']
         '''
         Here we upload step data and put it into data structure
         '''
+        parametrs = args['parametrs']
+        Step = args['Step']
         if self.load_flag:
             return 0
         k_boltz = 1.3806485279e-23
@@ -22,11 +22,16 @@ class energy(dash_object):
                 return x['c_peatom'] * e_hartry
         if self.type == 'full':
             def sum_f(x):
-                return (x['c_peatom'] + x['c_keatom']) * e_hartry
+                return ( x['c_peatom'] + x['c_keatom'] ) * e_hartry
 
         ion = parametrs[parametrs['type'] == 1.0].apply(sum_f, axis = 1).sum()
+        dist_x = parametrs[parametrs['type'] == 2.0]['x'].values[0] - parametrs[parametrs['type'] == 1.0]['x'].values[1]
+        dist_y = parametrs[parametrs['type'] == 2.0]['y'].values[0] - parametrs[parametrs['type'] == 1.0]['y'].values[1]
+        dist_z = parametrs[parametrs['type'] == 2.0]['z'].values[0] - parametrs[parametrs['type'] == 1.0]['z'].values[1]
+        dist = np.sqrt(dist_x**2 + dist_y**2 + dist_z**2)
+
         electron = parametrs[parametrs['type'] == 2.0].apply(sum_f, axis = 1).sum()
-        self.data.loc[len(self.data)] = [Step, ion, electron, ion + electron]
+        self.data.loc[len(self.data)] = [Step, dist, ion, electron, ion + electron]
 
     def __get_scatter_trace(self):
         '''
@@ -34,35 +39,30 @@ class energy(dash_object):
         :return:
         '''
         traces = []
-        if self.timestep == 0:
-            x_step = self.data['Step'].values
-            step = self.data.loc[self.current_index, 'Step']
-        else:
-            x_step = self.data['Step'].values * self.timestep
-            step = self.data.loc[self.current_index, 'Step'] * self.timestep
 
         traces.append(go.Scatter(
-            x=x_step,
-            y=self.data['ion'].values,
-            name='ions',
-            mode='line'
+            x = self.data['dist'].values,
+            y = self.data['ion'].values,
+            name = 'ions',
+            mode = 'line'
         ))
         traces.append(go.Scatter(
-            x=x_step,
+            x=self.data['dist'].values,
             y=self.data['electron'].values,
-            name='electrons',
-            mode='line',
+            name = 'electrons',
+            mode = 'line',
         ))
 
         traces.append(go.Scatter(
-            x=x_step,
+            x=self.data['dist'].values,
             y=self.data['all'].values,
             name='full',
-            mode='line'
+            mode = 'line'
         ))
         # create line in choosen step
         max_T = max(self.data['ion'].max(), self.data['electron'].max(), self.data['all'].max())
         min_T = min(self.data['ion'].min(), self.data['electron'].min(), self.data['all'].min())
+        step = self.data.loc[self.current_index, 'dist']
         traces.append(go.Scatter(
             x = np.linspace(step, step, 100),
             y = np.linspace(min_T, max_T, 100),
@@ -76,10 +76,6 @@ class energy(dash_object):
         if self.graph_type == 'scatter':
             return self.__get_scatter_trace()
     def __get_layout(self):
-        if self.timestep == 0:
-            x_title = "Step"
-        else:
-            x_title = "Time, fs"
         return go.Layout(
             showlegend = True,
             width=500,
@@ -89,7 +85,7 @@ class energy(dash_object):
                 showgrid = True,
                 zeroline = False,
                 showline = True,
-                title = x_title
+                title = 'Distance'
             ),
             yaxis=dict(
                 showgrid=True,
@@ -128,10 +124,9 @@ class energy(dash_object):
         return layout
 
 
-    def __init__(self, type = 'full', timestep=0):
-        self.timestep = timestep
+    def __init__(self, type = 'full'):
         self.type = type
-        self.data = pd.DataFrame(columns=["Step", "ion", "electron", "all"])
+        self.data = pd.DataFrame(columns=["Step", "dist", "ion", "electron", "all"])
         self.current_index = 0
         self.graph_type = 'scatter'
-        self.name = 'energy' + self.type
+        self.name = 'energy_dist' + self.type
