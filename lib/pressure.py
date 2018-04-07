7bc221002409db92179a5b32f395e0155dcadca4
IlyaFed <ilya.d.fedorov@phystech.edu>
2018-04-07 20:08:07 +0300

    add pressure graph

---
+++ lib/pressure.py
@@ -0,0 +1,95 @@
+# it's a common class of all system object
+from lib.common_object import *
+import numpy as np
+
+
+class pressure(dash_object):
+    def analyse(self, data, gen_info):
+        self.data = data
+        self.gen_info = gen_info
+        self.timestep = self.gen_info['timestep']
+        
+  
+
+    def __get_scatter_trace(self):
+        '''
+        Here we create scatter trace for graph
+        :return:
+        '''
+        traces = []
+
+        traces.append(go.Scatter(
+            x = self.data.index,
+            y = self.data['Press'].values,
+            name = 'Pressure',
+            mode = 'line'
+        ))
+
+        # create line in choosen step
+        max_T = max(self.data['Press'].max(), self.data['Press'].max())
+        min_T = min(self.data['Press'].min(), self.data['Press'].min())
+        step = self.current_index
+        traces.append(go.Scatter(
+            x = np.linspace(step, step, 100),
+            y = np.linspace(min_T, max_T, 100),
+            name = 'current step'
+        ))
+
+        return traces
+
+    def __get_trace(self):
+        if self.graph_type == 'scatter':
+            return self.__get_scatter_trace()
+    def __get_layout(self):
+        return go.Layout(
+            showlegend = True,
+            width=500,
+            height=300,
+            title = 'Pressure of system',
+            xaxis = dict(
+                showgrid = True,
+                zeroline = False,
+                showline = True,
+                title = 'Step'
+            ),
+            yaxis=dict(
+                showgrid=True,
+                zeroline=False,
+                showline=True,
+                title='Pressure, GPa'
+            )
+        )
+    def _update_graph(self):
+        '''
+        Here we update graph with neccessary parametrs
+        :return:
+        '''
+        traces = self.__get_trace()
+        layout = self.__get_layout()
+        return {
+            'data': traces,
+            'layout': layout
+        }
+
+    def _internal_callback(self):
+        ''''
+        Here we explain all callback which caused bu internal parametrs changes
+        '''
+
+    def get_html(self):
+        '''
+        Here we describe frontend of our object
+        '''
+        layout = html.Div(
+                    dcc.Graph(
+                        id=self.name,
+                        ),
+                    style = {'width': '49%', 'display': 'inline-block'}
+                    )
+        return layout
+
+    def __init__(self):
+        self.current_index = 0
+        self.graph_type = 'scatter'
+        self.name = 'pressure'
+        self.load_flag = 0
