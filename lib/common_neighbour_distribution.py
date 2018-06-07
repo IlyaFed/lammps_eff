39ae01544e17e04d291c9f6f309563bf2026bc63
IlyaFed <ilya.d.fedorov@phystech.edu>
2018-06-07 12:31:31 +0300

    add ability to change particle in neighbour analysis

--- lib/common_neighbour_distribution.py
+++ lib/common_neighbour_distribution.py
@@ -23,22 +23,23 @@ class common_neighbour_distribtution(dash_object):
         '''
         traces = []
 
-        for sys_info in self.info:
-            data = sys_info['data']
-            geninfo = sys_info['info']
-            filtrer_flag = 0
-            for filtrer in self.filtrer.split(';'):
-                if filtrer in geninfo['title']: #filtrered for rho TODO
-                    filtrer_flag += 1
-            if filtrer_flag == 0:
-                continue
-            N = data.loc[data.index[0], 'every'].shape[0]
-            traces.append(go.Scatter(
-                x=data['Press'].values[1:], # GPa
-                y=data['H2'].values[1:]/N*100, #[1:] - because first one is wrong
-                mode = 'line',
-                name = geninfo['title']
-            ))
+        for particle in self.particle:
+            for sys_info in self.info:
+                data = sys_info['data']
+                geninfo = sys_info['info']
+                filtrer_flag = 0
+                for filtrer in self.filtrer.split(';'):
+                    if filtrer in geninfo['title']: #filtrered for rho TODO
+                        filtrer_flag += 1
+                if filtrer_flag == 0:
+                    continue
+                N = data.loc[data.index[0], 'every'].shape[0]
+                traces.append(go.Scatter(
+                    x=data['Press'].values[1:], # GPa
+                    y=data[particle].values[1:]/N*100, #[1:] - because first one is wrong
+                    mode = 'line',
+                    name = particle + ", " + geninfo['title']
+                ))
 
 
           
@@ -96,9 +97,15 @@ class common_neighbour_distribtution(dash_object):
         @self.app.callback(
             dash.dependencies.Output(self.name, 'figure'),
             [dash.dependencies.Input(page, 'pathname'),
+             dash.dependencies.Input('common_neighbour_dropdown', 'value'),
              dash.dependencies.Input('common_neighbour_filtrer_button', 'n_clicks')],
             [dash.dependencies.State('common_neighbour_filtrer', 'value')])
-        def update_figure(pathname, n_clicks, filtrer):
+        def update_figure(pathname, dropdown, n_clicks, filtrer):
+            if type(dropdown) is list:        
+                self.particle = dropdown
+            elif type(dropdown) is str:
+                self.particle = [dropdown]
+
             if n_clicks:
                 self.filtrer = filtrer
             return self._update_graph()
@@ -110,6 +117,11 @@ class common_neighbour_distribtution(dash_object):
         layout = html.Div([
             dcc.Input(id='common_neighbour_filtrer', type='Filtrer (;)', value=self.filtrer),
             html.Button(id='common_neighbour_filtrer_button', n_clicks=0, children='Filtrer'),
+            dcc.Dropdown(id='common_neighbour_dropdown',
+                options=[{'label': name, 'value': name} for name in self.particle_types],
+                multi=True,
+                value='H2'
+            ),
             html.Div(dcc.Graph(
                 id=self.name,
             ))],
@@ -123,6 +135,8 @@ class common_neighbour_distribtution(dash_object):
         self.filtrer = filtrer
         self.current_index = 0
         self.graph_type = 'scatter'
+        self.particle_types = ["e", "H+", "H", "H2+", "H2", "H3+", "H3", "other"]
+        self.particle = ['H2']
         self.name = 'Common Neighbour distribution'
         self.yaxis_type = 'linear'
         self.load_flag = 0
